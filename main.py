7f0a6309256da0de5155aca8b657686e162c45d2
danardvincente <danardvincente1@gmail.com>
2025-04-14 20:32:29 -0700

    Added changed from Ch2/L3 to Ch2/L8

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import pygame
 from constants import *
-
+from player import *
 
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	clock = pygame.time.Clock()
+	dt = 0
+
 	print("Starting Asteroids!")
 
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)  # Player.containers dynamically create containers as static variable
+
+	the_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	
+
+
 	while True:
 
 		# This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
@@ -16,8 +28,23 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 			
+		# the_player.update(dt) # moves player left/right etc. based on key input [wasd]
+		updatable.update(dt) # using pygame groups to manage game object
+
 		screen.fill("black")
-		pygame.display.flip()
+
+		
+		# re-render the player on the screen each frame
+		# the_player.draw(screen)
+		for f in drawable:
+			# using pygame groups to manage game object
+			f.draw(screen)
+
+		pygame.display.flip() # refreshes the screen
+
+		amt_time_loop = clock.tick(60) # returns he amount of time that has passed since the last time it was called: the delta time
+		dt = amt_time_loop / 1000 # converts milliseconds from amt_time_loop to seconds (delta time in seconds)
+
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,57 @@
+import pygame
+from circleshape import *
+from constants import *
+
+
+class Player(CircleShape):
+
+
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.x = x
+        self.y = y
+        self.rotation = 0
+
+    # note self.position is an attribute in the parent class CircleShape
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+
+    def move(self, dt):
+
+        """ 
+        We start with a unit vector pointing straight up from (0, 0) to (0, 1).
+            We rotate that vector by the player's rotation, so it's pointing in the direction the player is facing.
+            We multiply by PLAYER_SPEED * dt. A larger vector means faster movement.
+            Add the vector to our position to move the player.
+
+        """
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
